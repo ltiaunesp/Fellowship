e156f422e378e77fb4773ad2e864298ac2ddde05
Mateus Etto <mateus.etto@gmail.com>
2016-02-20 14:31:55 -0200

    added basic controllers and functions headers

---
+++ www/js/modules/profiles/controllers/ProfileController.js
@@ -0,0 +1,19 @@
+angular.module('fellowship.modules.profiles.controllers')
+  .controller('ProfileController', [function ($scope) {
+
+    // List users
+    $scope.listUsers = function() {
+      console.log('LISTING USERS');
+    }
+
+    // Get user profile
+    $scope.getProfile = function(id) {
+      console.log('GETTING USER ID = ' + id);
+    }
+
+    // Set user profile
+    $scope.setProfile = function() {
+      console.log('sETTING USER ID = ' + id);
+    }
+
+  }])
