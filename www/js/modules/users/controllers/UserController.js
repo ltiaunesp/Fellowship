7333320865553650885f3ac399ea084995f4e3ef
Mateus Etto <mateus.etto@gmail.com>
2016-02-20 16:00:16 -0200

    getting info on creating account

--- www/js/modules/users/controllers/UserController.js
+++ www/js/modules/users/controllers/UserController.js
@@ -18,7 +18,20 @@ angular.module('fellowship.modules.users.controllers')
 
     // User registration
     $scope.register = function() {
-      console.log('REGISTER USER');
+      // Taking the account information
+      var firstName = document.querySelector("#first-name").value;
+      var lastName = document.querySelector("#last-name").value;
+      var username = document.querySelector("#username").value;
+      var organizationName = document.querySelector("#org-name").value;
+      var password = document.querySelector("#password").value;
+      var confirmPassword = document.querySelector("#conf-password").value;
+
+      if (password !== confirmPassword) {
+        console.log('Password dont match!');
+        return;
+      }
+
+      console.log('Account created!')
     }
 
   });
