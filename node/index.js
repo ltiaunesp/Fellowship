133e81f76cc325a84f7795c293d3a1df4fec586d
vinifig <vinifig@hotmail.com>
2016-02-21 03:09:31 -0300

    init server

--- node/index.js
+++ node/index.js
@@ -8,10 +8,12 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/api/:path", function(req,res){
-  fs.readFile('./json/' + req.params.path, function(err, data){
+  fs.readFile('./json/' + req.params.path + '.json', function(err, data){
     if(err)
       res.end();
     else
       res.end(data);
   });
 })
+
+app.listen(8080);
