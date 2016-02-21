d790fd525e7957b3a649962bdd5134533f190ae8
Mateus Etto <mateus.etto@gmail.com>
2016-02-21 00:47:54 -0300

    user controller added

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -1,1 +1,13 @@
-angular.module('fellowship.controllers', ['fellowship.modules']);
+angular.module('fellowship.controllers', ['fellowship.modules'])
+  .controller('AppController', function($scope, $location, UserService) {
+
+    UserService.verifyLogin()
+    .then( (result) => {
+      console.log('Logged in!');
+      // Redirect to our home.html
+      $location.url("home.html");
+    }, (data, status) => {
+      console.log('Please log in.');
+      $location("login.html");
+    });
+  });
